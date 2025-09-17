8bf9d884c1239f9247256b55de6c7a7267e52c11
julinha <jucalemossouza@gmail.com>
2025-09-17 12:14:11 -0300

    a

---
+++ Assets/DungeonGame/Scripts/MenuInicial.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicial : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
